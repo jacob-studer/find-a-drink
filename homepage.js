71fbd81511f692f8882edce049e51b90695a35e6
Jacob Studer <jacob.studer.dev@gmail.com>
2021-05-28 12:23:46 -0600

    updates

---
+++ homepage.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("#homepage-search");
+const button = document.querySelector('#search-btn');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+    console.log("works");
+    const searchInputVal = document.querySelector('#homepage-search').value;
+    localStorage.setItem('searchQuery', JSON.stringify(searchInputVal));
+
+    if (searchInputVal === "")  {
+        return(handleSearchFormSubmit);
+        }
+
+        document.location.replace('drinksearch.html');
+}
+
+
+button.addEventListener('click', handleSearchFormSubmit);
+
+// module.exports = { drinkSearch };
